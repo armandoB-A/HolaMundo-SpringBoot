9d5df30b879e91d5288ef96dd8bd81de39971f02
Armando <blasarcej@gmail.com>
2021-07-16 23:48:37 -0500

    Saludo personalizado

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,8 +14,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String name() {
-		return "saludos crack";
+	@GetMapping("/saludo-personalizado")
+	public String name(@RequestParam(value = "nombre") String nombre) {
+		return "salu2 "+nombre;
 	}
 }
